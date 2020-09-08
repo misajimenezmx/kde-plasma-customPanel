0046e1514850237e849f5c1de203e29279670bd2
Misael Jiménez <misxtoni@tuta.io>
2020-09-08 10:07:24 -0500

    Fix whe simplemenu widtget not exist

--- contents/layout.js
+++ contents/layout.js
@@ -2,9 +2,14 @@ var panel = new Panel
 panel.location = "top";
 panel.height = Math.round(gridUnit * 1.5);
 
-var simplemenu = panel.addWidget("org.kde.plasma.simplemenu")
+var simplemenu
+if(knownWidgetTypes.indexOf('org.kde.plasma.simplemenu') > -1){
+    simplemenu = panel.addWidget("org.kde.plasma.simplemenu")
+}else{
+    simplemenu = panel.addWidget("org.kde.plasma.kickoff")
+}
 simplemenu.currentConfigGroup = ["Shortcuts"]
-simplemenu.writeConfig("global", "Meta")
+simplemenu.writeConfig("global", "Alt+F1")
 
 panel.addWidget("org.kde.plasma.appmenu");
 panel.addWidget("org.kde.plasma.panelspacer") 
